60542926e056374e0b2ae0bfc84b107fc3c7d55a
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-10-06 04:19:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 function tocaSom(seletorAudio) {
-   const elemento = document.querySelector(seletorAudio)
+    const elemento = document.querySelector(seletorAudio)
 
-   if(elemento===null){
-    alert =('erro')
-   }
-   if(elemento!=null&&){
-    
-    if(elemento.localName === 'audio'){
+
+    if (elemento != null && elemento.localName === 'audio') {
+
         elemento.play()
+    }else{
+        console.log('Elemento/seletor nao encontrado')
     }
- 
-    
-   }
-   
+
+
 }
+   
+
 
 // selecionar todos os elementos com a class .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -43,7 +42,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
             if (evento.code == "space" || "enter") {
                 tecla.classList.add('ativa')
             }
-            
+
 
         }
         tecla.onkeyup = function () {
